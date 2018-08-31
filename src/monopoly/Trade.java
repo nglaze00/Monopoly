e2cdc5b2904d89f1dd03e959b1ba275b506b683c
nglaze00 <nglaze00@gmail.com>
2018-08-30 22:32:45 -0500

    Began working on trades; added proposing money

--- src/monopoly/Trade.java
+++ src/monopoly/Trade.java
@@ -9,6 +9,36 @@ package monopoly;
  *
  * @author nglaz
  */
+
+import java.util.ArrayList;
 public class Trade {
-    
+	private Player proposer;
+	private Player recipient;
+	private int[] money;
+	private ArrayList<Property> properties;
+	
+	public Trade(Player proposer, Player recipient) {
+		this.proposer = recipient;
+		this.recipient = recipient;
+		this.money = new int[2];
+		this.properties = new ArrayList<Property>();
+	}
+	
+	public boolean proposePaidCash(int money) {
+		if(money > proposer.money()) {
+			System.out.println("Not enough money");
+			return false;
+		}
+		this.money[0] = money;
+		return true;
+	}
+	public boolean proposeRecievedCash(int money) {
+		if(money > recipient.money()) {
+			System.out.println("Not enough money");
+			return false;
+		}
+		this.money[1] = money;
+		return true;
+	}
+	public void proposeProperty
 }
